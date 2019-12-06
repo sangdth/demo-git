d3dcf9abc8345696951d0cfd1707e7319dfd72b4
Phuc <chauminhphuc1994it@gmail.com>
2019-12-07 00:51:27 +0700

    Thay doi tròngile app :"logo" va index: 10px

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hehehe. Day la logo.
+        Hehehe. Day la logo. logo 2
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. tessttt
